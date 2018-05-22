5655186038dd401a83323ba9126e86cd1042d2a1
Sara Jade <v-sajade@microsoft.com>
2018-05-21 22:07:40 -0700

    Add TreeNode and ListNode classes

--- Trees/Program.cs
+++ Trees/Program.cs
@@ -6,6 +6,32 @@ using System.Threading.Tasks;
 
 namespace Trees
 {
+    class TreeNode<T>
+    {
+        T data;
+        TreeNode<T> left;
+        TreeNode<T> right;
+
+        public TreeNode(T data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    class ListNode<T>
+    {
+        T data;
+        ListNode<T> next;
+
+        public ListNode(T data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
