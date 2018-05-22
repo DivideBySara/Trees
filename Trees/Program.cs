aedf35550d0898ad372b9e6899b83453e2c9296a
Sara Jade <v-sajade@microsoft.com>
2018-05-22 00:13:18 -0700

    Add create a bst

--- Trees/Program.cs
+++ Trees/Program.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Trees
 {
-    class TreeNode<T>
+    public class TreeNode<T>
     {
-        T data;
-        TreeNode<T> left;
-        TreeNode<T> right;
+        public T data { get; set; }
+        public TreeNode<T> left { get; set; }
+        public TreeNode<T> right { get; set; }
 
         public TreeNode(T data)
         {
@@ -20,10 +20,10 @@ namespace Trees
         }
     }
 
-    class ListNode<T>
+    public class ListNode<T>
     {
-        T data;
-        ListNode<T> next;
+        public T data { get; set; }
+        public ListNode<T> next { get; set; }
 
         public ListNode(T data)
         {
@@ -36,6 +36,26 @@ namespace Trees
     {
         static void Main(string[] args)
         {
+            // 21. Given a BST, create a linkedlist of all the nodes at each depth
+            // Q's: Is the tree balanced? How deep is it? 
+            // Input = a bst?
+            // Output = a list of linked lists?
+            // Should I write my TreeNode and ListNode classes? Handle edge cases?
+
+            Console.WriteLine("Given a BST, create a linked list of all the nodes at each depth.");
+
+            // 1. create bst
+            int size = 7;
+            TreeNode<int> root = GetBst(size);            
+        }
+
+        private static TreeNode<int> GetBst(int size)
+        {
+            TreeNode<int> root = new TreeNode<int>(1);
+
+            // https://stackoverflow.com/questions/828398/how-to-create-a-binary-tree for more details
+
+            return root;
         }
     }
 }
