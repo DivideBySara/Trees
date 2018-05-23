e1c76a0dce915044add33a54d5e4f4439227085e
Sara Jade (WTIA Workforce Institute) <v-sajade@microsoft.com>
2018-05-22 22:25:43 -0700

    Create skeleton for #21: 21. Given a BST, create a linkedlist of all the nodes at each depth

--- Trees/Program.cs
+++ Trees/Program.cs
@@ -8,27 +8,27 @@ namespace Trees
 {
     public class TreeNode<T>
     {
-        public T data { get; set; }
-        public TreeNode<T> left { get; set; }
-        public TreeNode<T> right { get; set; }
+        public T Data { get; set; }
+        public TreeNode<T> Left { get; set; }
+        public TreeNode<T> Right { get; set; }
 
         public TreeNode(T data)
         {
-            this.data = data;
-            this.left = null;
-            this.right = null;
+            this.Data = data;
+            this.Left = null;
+            this.Right = null;
         }
     }
 
     public class ListNode<T>
     {
-        public T data { get; set; }
-        public ListNode<T> next { get; set; }
+        public T Data { get; set; }
+        public ListNode<T> Next { get; set; }
 
         public ListNode(T data)
         {
-            this.data = data;
-            this.next = null;
+            this.Data = data;
+            this.Next = null;
         }
     }
 
@@ -45,15 +45,44 @@ namespace Trees
             Console.WriteLine("Given a BST, create a linked list of all the nodes at each depth.");
 
             // 1. create bst
-            int size = 7;
-            TreeNode<int> root = GetBst(size);            
+            Console.WriteLine("\n1. Create binary tree.");
+            TreeNode<string> root = GetBst();
+
+            // 2. find the depth of the tree
+            Console.WriteLine("2. Find the depth of the tree.");
+            int depth = GetBstDepth(root);
+
+            // 3. create a list of linked lists
+            List<ListNode<string>> listOfListNodes = GetListOfListNodes(depth);
+
+            // 4. print list of linked lists to confirm
+            PrintListOfListNodes(listOfListNodes);
         }
 
-        private static TreeNode<int> GetBst(int size)
+        private static void PrintListOfListNodes(List<ListNode<string>> listOfListNodes)
         {
-            TreeNode<int> root = new TreeNode<int>(1);
+            throw new NotImplementedException();
+        }
 
-            // https://stackoverflow.com/questions/828398/how-to-create-a-binary-tree for more details
+        private static List<ListNode<string>> GetListOfListNodes(int depth)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int GetBstDepth(TreeNode<string> root)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static TreeNode<string> GetBst()
+        {
+            TreeNode<string> root = new TreeNode<string>("d");
+            root.Left = new TreeNode<string>("b");
+            root.Right = new TreeNode<string>("c");
+            root.Left.Left = new TreeNode<string>("a");
+            root.Left.Right = new TreeNode<string>("f");
+            root.Right.Left = new TreeNode<string>("e");
+            root.Right.Right = new TreeNode<string>("g");
 
             return root;
         }
