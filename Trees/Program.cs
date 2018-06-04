4845dde6557413b2cf4f6bc80148d2a3a115c531
Sara Jade <v-sajade@microsoft.com>
2018-06-04 10:21:36 -0700

    Finish pseudocode for #3

--- Trees/Program.cs
+++ Trees/Program.cs
@@ -74,6 +74,14 @@ namespace Trees
 
             // 3. You have 2 very large binary trees: T1 with millions of nodes, and T2 with hundreds of nodes. Create an algorithm to decide if T2 is a subtree of T1. A tree T2 is a subtree of T1 if there exists a node n in T1 such that the subtree of n is identical to T2. That is, if you cut off the tree at node n, the two trees would be identical.
             // Q: input = rootA & rootB? Output = boolean.
+            // A) bool isEqualTreeLeft = false;
+            //    bool isEqualTreeRight = false;
+            // B) isEqualTreeLeft = SubtreeExists(rootT2, rootT1.Left);
+            //    isEqualTreeRight = SubtreeExists(roott2, rootT2.Right);
+            // C) if either bool is true, return true, else call recursively with rootT1.Left, rootT1.Right
+            // D) return false at end of method 
+
+            // 4. You are given a binary tree in which each node contains a value. Design an algorithm to print all paths which sum to a given value. Note that a path can start or end anywhere in the tree.
 
             Console.ReadKey();
         }
